5943197647ca89b7729116409d779caf6505058c
tomvsaji <tomvellavoor@gmail.com>
2023-03-11 22:35:09 +0300

    chore: move components file out

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,11 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '@/styles/Home.module.css'
-import Header from './components/header'
-import Navbar from './components/navbar'
-import Projects from './components/projects'
-import Contact from './components/contact'
-import Footer from './components/footer'
+import Header from '@/components/header'
+import Navbar from '@/components/navbar'
+import Projects from '@/components/projects'
+import Contact from '@/components/contact'
+import Footer from '@/components/footer'
 
 export default function Home() {
   return (
